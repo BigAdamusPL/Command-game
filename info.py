ad1b91c224b86ff6377b3a94116bc961b2e12d8e
BigAdamusPL <156455913+BigAdamusPL@users.noreply.github.com>
2025-02-24 19:51:16 +0100

    info.py

--- info.py
+++ info.py
@@ -5,9 +5,8 @@ def GetGameInfo(GameName):
     try:
         url = 'https://store.steampowered.com/search/?term='
         url2 = 'https://store.steampowered.com/app/'
-        url3 = 'https://steamcharts.com/app/' #'https://steamcharts.com/search/?q='
-        #url2 = 'https://steamdb.info/search/?a=all&q=%2F'
-        #url3 = "https://steamdb.info/app/1721470/charts/"
+        url3 = 'https://steamcharts.com/app/'
+        
         response = requests.get(url + GameName)
         emoticons_dictionary = {
             "Overwhelmingly Positive": " :star_struck:",
@@ -22,20 +21,16 @@ def GetGameInfo(GameName):
         }
 
         if response.status_code != 200:
-            #print('Nie udało się otrzymać danych')
             return "Error"
         else:
             soup = BeautifulSoup(response.content, 'html.parser')
             games = soup.find_all("a", class_ = 'search_result_row')
-            #print(games[0].get('data-ds-appid'))
-            app_ids = [] #games.get('data-ds-appid')
+            app_ids = []
             app_names = []
             app_id = None
             
             app_names = [game.find('span', attrs={'class':'title'}).text.strip() for game in games if game.find('span', attrs={'class':'title'})]
             app_ids = [ids.get("data-ds-appid") for ids in games]
-            
-            #print(app_names, app_ids)
 
             for i in range(len(app_names)):
                 if app_names[i].lower() == GameName.lower():
@@ -43,15 +38,11 @@ def GetGameInfo(GameName):
                     break
             if app_id == None:
                 app_id = app_ids[0]
-            
-            #print(f"{app_id} -----------------------------------------------------")
 
             if app_id != None:
-                #print('\n----------znaleziono grę-------------\n')
                 response = requests.get(url2 + app_id)
 
                 if response.status_code != 200:
-                    #print('Nie udało się otrzymać danych')
                     return "Error"
                 else:
                     soup = BeautifulSoup(response.content, "html.parser")
@@ -101,23 +92,17 @@ def GetGameInfo(GameName):
                             players_online = format(int(players_online), ',').replace(',', ' ')
                     except:
                         players_online = "Couldn't find online players"
+                    
                     #print(f"{name}\n{price}\n{before_price}\n{description}\n{release_date}\n{studio}\n{image}\n{opinion}\n\n{url2+app_id}")
                     
                     return [name, description, price, before_price, release_date, studio, opinion, image, url2+app_id, price_procent, players_online]
                     
 
             else:
-                #print('nie znaleziono gier')
                 return "Game not found"
-            
-            #print("\n----------------------------------------\n")
-
-            #print(games)
-            #print(f"AppIds: {app_ids}")
     except:
         return "Error"
         
-while False:
+while False: # --------------------------- if you want to try it here, then change ,,False'' with ,,True'' here ----------------------------------------
     x = input("Game: ")
     print(GetGameInfo(x))
-#<div class="_1n_4-zvf0n4aqGEksbgW9N">Counter-Strike 2</div>
